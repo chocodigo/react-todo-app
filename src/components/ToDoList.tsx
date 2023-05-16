077533d2fd088fcc1bd2397c95c82a702f910930
chocodigo <chocodigo>
2023-05-16 22:36:14 +0900

    첫번째 커밋

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  atom,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+interface IForm {
+  toDo: string;
+}
+
+interface IToDo {
+  text: string;
+  id: number;
+  category: "DONE" | "DOING" | "TO_DO";
+}
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, category: "TO_DO", id: Date.now() },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "Please write a To Do",
+          })}
+          placeholder={"Write a to do"}
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
